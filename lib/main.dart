ff3332bfddbb9129e759b12d56eaf81bc8f72f43
Jumoke Olaleye <jumokeolaleye0@gmail.com>
2024-10-09 16:17:46 +0100

    portrait up and debug banner removed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:monipoint_animated_app/view/screens/bottom_navigator.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
@@ -16,7 +20,8 @@ class MyApp extends StatelessWidget {
         designSize: const Size(360, 720),
         builder: (context, child) {
           return MaterialApp(
-            title: 'Flutter Demo',
+            title: 'Monipoint Animated',
+            debugShowCheckedModeBanner: false,
             theme: ThemeData(
               fontFamily: 'Euclid Circular A',
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -27,5 +32,3 @@ class MyApp extends StatelessWidget {
         });
   }
 }
-
-
